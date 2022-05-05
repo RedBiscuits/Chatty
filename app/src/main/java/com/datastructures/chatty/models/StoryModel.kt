4e5b6a895640e2b96e9a36081458f0961834e69e
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2022-05-05 09:07:29 +0200

    added images and storage

---
+++ app/src/main/java/com/datastructures/chatty/models/StoryModel.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.models
+
+class StoryModel() {
+    var uri : String = ""
+    var description: String? = null
+    var date: String = ""
+
+    constructor( uri: String , description: String? , date : String) : this(){
+        this.uri = uri
+        this.description = description
+        this.date = date
+    }
+}
