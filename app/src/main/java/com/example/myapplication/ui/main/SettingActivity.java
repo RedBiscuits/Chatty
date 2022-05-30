8a7854089ee7fb10ecdad890791b9013cd8d2fbf
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2022-05-30 14:04:23 +0200

    Fixed setting image and login glitch //Dont fkn ignore changes in new pushes

--- app/src/main/java/com/example/myapplication/ui/main/SettingActivity.java
+++ app/src/main/java/com/example/myapplication/ui/main/SettingActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -83,7 +84,7 @@ public class SettingActivity extends AppCompatActivity {
                     diskCacheStrategy(DiskCacheStrategy.ALL)
                     .into(userImage);
         }
-
+        userImage.setImageURI(Uri.parse(imageURL));
         usernameTextView.setText(username);
         phoneTextView.setText(phone);
         bioTextView.setText(bio);
